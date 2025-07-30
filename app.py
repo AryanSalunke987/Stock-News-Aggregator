da0c093d6811b43629bd646007c3a36d060b944f
Aryan Salunke <165202122+AryanSalunke987@users.noreply.github.com>
2025-07-30 10:59:52 +0530

    Update2 app.py

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ import streamlit as st
 import pandas as pd
 from scrapper import scrape_all
 from datetime import datetime
+import pytz
 
-st.set_page_config(page_title="Stock Market News Aggregator", layout="wide")
+ist = pytz.timezone('Asia/Kolkata') #timezone to india
+ist_time = datetime.now(ist)
+
+st.set_page_config(page_title="Stock Market News Aggregator", layout="wide") #layout
 
 st.title("📈 Stock Market News Aggregator")
-st.markdown("Aggregates headlines from **Moneycontrol** and **Economic Times**.")
+st.markdown("Aggregates headlines from **Moneycontrol** and **Economic Times**.") #title
 
 if st.button("Scrape Latest News"):
     with st.spinner("Scraping news articles..."):
-        df = scrape_all()
+        df = scrape_all() #calls scrapper.py
         st.success(f"Scraped {len(df)} articles!")
         st.dataframe(df[['source', 'headline', 'summary', 'link', 'timestamp']])
 else:
     st.info("Click the button above to scrape the latest news.")
 
 if st.checkbox("Show saved CSV"):
-    df = pd.read_csv("headlines.csv")
+    df = pd.read_csv("headlines.csv") #for csv creation
     st.dataframe(df[['source', 'headline', 'summary', 'link', 'timestamp']])
 
-st.caption(f"🕒 Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+st.caption(f"🕒 Last updated: {ist_time.strftime('%Y-%m-%d %H:%M:%S')} IST") #caption date and time at bottom
